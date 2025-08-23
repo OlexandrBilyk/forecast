6ac7ab43be18a6f7707bfca543098e894fe7732f
OlexandrBilyk <0500423510s@gmail.com>
2025-08-23 10:02:28 +0300

    fix news bugs

--- src/redux/news/newsApi.js
+++ src/redux/news/newsApi.js
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { format } from "date-fns";
+import { format, subDays } from "date-fns";
 
-const apiKey = import.meta.env.VITE_NEWS_API_KEY
+const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+
 const today = format(new Date(), "yyyy-MM-dd");
+const yesterday = format(subDays(new Date(), 1), "yyyy-MM-dd");
 
 export const newsApi = createApi({
     reducerPath: "newsApi",
@@ -13,11 +15,10 @@ export const newsApi = createApi({
     endpoints: (builder) => ({
         getNews: builder.query({
             query: ({ page = 1, limit = 4 }) =>
-                `everything?q=Apple&from=${today}&page=${page}&pageSize=${limit}&apiKey=${apiKey}`,
+                `everything?q=Apple&from=${yesterday}&to=${today}&sortBy=publishedAt&page=${page}&pageSize=${limit}&apiKey=${apiKey}`,
             providesTags: ["news"],
         }),
     }),
 });
 
-
-export const { useLazyGetNewsQuery } = newsApi
+export const { useLazyGetNewsQuery } = newsApi;
